24081d581770ff3d9e9b4b4553440fc610a29dd7
nikit <nikitenkogrigory@gmail.com>
2021-01-12 18:40:55 +0700

    colored ver.

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
+from UI import Ui_MainWindow
 import random
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.pushButton.clicked.connect(self.run)
 
@@ -27,7 +28,10 @@ class MyWidget(QMainWindow):
     def draw_flag(self, qp):
         x, y = random.randint(0, 500), random.randint(0, 400)
         d = random.randint(0, 200)
-        qp.setBrush(QColor(255, 216, 0))
+        a = random.randint(0, 255)
+        b = random.randint(0, 255)
+        c = random.randint(0, 255)
+        qp.setBrush(QColor(a, b, c))
         qp.drawEllipse(x, y, d, d)
 
 
